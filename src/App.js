dc3286488d880909fe46d457f8896676d3c6fee0
noueliah <noelittz@gmail.com>
2022-11-09 17:02:24 -0300

    adding changes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 
 import './App.css';
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+import { ShowAlertComponent } from './components/Container';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <>
 
+      <header className="App-header">
         <p>
           This is a paragraph in App.js
         </p>
+        <button onClick={() => alert('Thanks for clicking!')}>Click here</button>
       </header>
-    </div>
+      <ShowAlertComponent />
+    </>
   );
 }
 
---
+++ src/components/Container.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShowAlertComponent = () => {
+    const showAlert = () => {
+        alert("I'm an alert");
+    }
+
+    return <button onClick={showAlert}>Show alert</button>;
+}
+
+export { ShowAlertComponent };
