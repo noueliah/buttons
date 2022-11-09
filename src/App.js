d3389cb19218f3f3ac0c16562d639c8b2b3e600d
noueliah <noelittz@gmail.com>
2022-11-09 16:40:00 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+
+        <p>
+          This is a paragraph in App.js
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
