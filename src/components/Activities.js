669be1e170c140ef320e57c05b14d948d22bd87b
noueliah <noelittz@gmail.com>
2022-12-29 12:57:59 -0300

    working version with 'Loading' message

--- src/components/Activities.js
+++ src/components/Activities.js
@@ -3,15 +3,23 @@ import { useEffect, useState } from "react";
 
 function Activities() {
   function loadActivity() {
+    setIsLoading(true);
     fetch("http://www.boredapi.com/api/activity")
       .then((response) => response.json()) // esto lo convierte a json
-      .then((data) => setActivity(data.activity));
+      .then((data) => {
+        setActivity(data.activity);
+        setIsLoading(false);
+      });
   }
 
   const [activity, setActivity] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
     loadActivity();
   }, []);
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
   return (
     <div>
       <h1>{activity}</h1>
